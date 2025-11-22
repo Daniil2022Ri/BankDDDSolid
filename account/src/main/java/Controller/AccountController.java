38f2d7f2375df040163581a469afb174703a7b50
Darkness <mr.deni_2022@mail.ru>
2025-11-22 22:04:57 +0300

    Fixe Controller

--- account/src/main/java/Controller/AccountController.java
+++ account/src/main/java/Controller/AccountController.java
@@ -1,13 +1,11 @@
 package Controller;
 
 import dto.AccountDto;
-import dto.AuditDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.AccountService;
-import service.AuditService;
 
 import java.util.List;
 
@@ -18,25 +16,27 @@ import java.util.List;
 public class AccountController {
     private final AccountService accountService;
 
-    //============================= Controller Account ==========================
     @PostMapping
-    public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto accountDto ,
-                                                    @RequestHeader("user_id") String userId){
-        AccountDto createAccount = accountService.createAccount(accountDto , userId);
-        return new ResponseEntity<>(createAccount , HttpStatus.CREATED);
+    public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto accountDto,
+                                                    @RequestHeader("user_id") String userId) {
+        AccountDto createdAccount = accountService.createAccount(accountDto, userId);
+        return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
     }
+
     @GetMapping("/accounts/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
         return accountService.findById(id)
                 .map(account -> new ResponseEntity<>(account, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
     @GetMapping
     public ResponseEntity<List<AccountDto>> getAllAccounts() {
         List<AccountDto> accounts = accountService.findAll();
         return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
-    @PutMapping("accounts/{id}")
+
+    @PutMapping("/accounts/{id}")
     public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id,
                                                     @RequestBody AccountDto accountDto,
                                                     @RequestHeader("user_id") String userId) {
@@ -48,7 +48,7 @@ public class AccountController {
         }
     }
 
-    @DeleteMapping("accounts/{id}")
+    @DeleteMapping("/accounts/{id}")
     public ResponseEntity<Void> deleteAccount(@PathVariable Long id,
                                               @RequestHeader("user_id") String userId) {
         boolean deleted = accountService.deleteAccount(id, userId);
@@ -56,68 +56,21 @@ public class AccountController {
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/accounts/{entityType}")
+    @GetMapping("/entity-type/{entityType}")
     public ResponseEntity<List<AccountDto>> getAccountsByEntityType(@PathVariable String entityType) {
         List<AccountDto> accounts = accountService.findByEntityType(entityType);
         return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
 
-    @GetMapping("/accounts/{createdBy}")
+    @GetMapping("/created-by/{createdBy}")
     public ResponseEntity<List<AccountDto>> getAccountsByCreator(@PathVariable String createdBy) {
         List<AccountDto> accounts = accountService.findByCreatedBy(createdBy);
         return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
 
-    @GetMapping("/accounts/{operationType}")
+    @GetMapping("/operation-type/{operationType}")
     public ResponseEntity<List<AccountDto>> getAccountsByOperationType(@PathVariable String operationType) {
         List<AccountDto> accounts = accountService.findByOperationType(operationType);
         return new ResponseEntity<>(accounts, HttpStatus.OK);
-    }
-
-    //============================= Controller Audit ==========================
-
-    private final AuditService auditService;
-
-    @PostMapping
-    public ResponseEntity<AuditDto> createAudit(@RequestBody AuditDto auditDto) {
-        AuditDto createdAudit = auditService.createAudit(auditDto);
-        return new ResponseEntity<>(createdAudit, HttpStatus.CREATED);
-    }
-
-    @GetMapping("accounts/{id}")
-    public ResponseEntity<AuditDto> getAuditById(@PathVariable Long id) {
-        return auditService.findAuditById(id)
-                .map(audit -> new ResponseEntity<>(audit, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<AuditDto>> getAllAudits() {
-        List<AuditDto> audits = auditService.findAllAudits();
-        return new ResponseEntity<>(audits, HttpStatus.OK);
-    }
-
-    @GetMapping("/password/{passwordId}")
-    public ResponseEntity<List<AuditDto>> getAuditsByPasswordId(@PathVariable Long passwordId) {
-        List<AuditDto> audits = auditService.findByPasswordId(passwordId);
-        return new ResponseEntity<>(audits, HttpStatus.OK);
-    }
-
-    @GetMapping("/account-number/{accountNumber}")
-    public ResponseEntity<List<AuditDto>> getAuditsByAccountNumber(@PathVariable Long accountNumber) {
-        List<AuditDto> audits = auditService.findByAccountNumber(accountNumber);
-        return new ResponseEntity<>(audits, HttpStatus.OK);
-    }
-
-    @GetMapping("/profile/{profileId}")
-    public ResponseEntity<List<AuditDto>> getAuditsByProfileId(@PathVariable Long profileId) {
-        List<AuditDto> audits = auditService.findByProfileId(profileId);
-        return new ResponseEntity<>(audits, HttpStatus.OK);
-    }
-
-    @GetMapping("/negative-balance/{negativeBalance}")
-    public ResponseEntity<List<AuditDto>> getAuditsByNegativeBalance(@PathVariable boolean negativeBalance) {
-        List<AuditDto> audits = auditService.findByNegativeBalance(negativeBalance);
-        return new ResponseEntity<>(audits, HttpStatus.OK);
     }
 }
